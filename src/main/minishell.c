38af22d535c90f217c69516e3c4c814f977b38d8
sogabrie <gabrielyan.sos@mail.ru>
2023-03-31 19:58:00 +0400

    update

--- src/main/minishell.c
+++ src/main/minishell.c
@@ -7,14 +7,14 @@ int main(int argc, char **argv, char **envp)
 
 	my_envp = replace_envp(envp);
 	int i = 0;
-	while (my_envp[i])
-		printf("%s",my_envp[i++]);
+	while (envp[i])
+		printf("%s\n",envp[i++]);
 	if (argc >  1)
 		return (2);
     while(1)
 	{
     	line = readline("minishell-1.0$ ");
-		if (!line)
+		if (line)
 		{
 			add_history(line);
 			// main_parsing(line, my_envp);
