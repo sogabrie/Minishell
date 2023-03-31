0d9a8cddc7191fbed8935a36eb5562a09bdc410e
ZoktrFall <shahavetgnum@gmail.com>
2023-04-01 00:15:57 +0400

    add here_doc

--- includes/libft.h
+++ includes/libft.h
@@ -34,6 +34,7 @@ int		ft_tolower(int a);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strcmp_flag(char *str1, char *str2, int flag);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
