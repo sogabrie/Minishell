3cceae659b09b4bfdedba5b5d5749c7f21fd27e2
ZoktrFall <shahavetgnum@gmail.com>
2023-04-02 19:01:36 +0400

    update control

--- src/my_exe/wildcards.c
+++ src/my_exe/wildcards.c
@@ -1,44 +1,45 @@
 #include "minishell.h"
 
-int		check_suitable(char *name, char *wild_string)
+int	check_suitable(char *object_name, char *wild_string)
 {
-	int	i;
-	int	j;
-	int len;
+	// size_t	i;
+	// size_t	j;
+	// size_t	wild_flag;
 
-	i = 0;
-	j = 0;
-	len = ft_strlen(name);
-	if(i == 0 && wild_string[i] != '*')
-	{
-		while(wild_string[i] != '*')
-			if(name[j++] != wild_string[i++])
-				return (1);
-		if(name[j] == '\0' && wild_string[i + 1] != '\0')
-			return (1);
-		if(name[j] != '\0' && wild_string[i + 1] == '\0')
-			return (0);
-	}
-	while(wild_string[i] != '\0')
-	{
-		while(wild_string[i] == '*' && wild_string[i] != '\0')
-			i++;
-		if(name[j] != '\0' && wild_string[i] == '\0')
-			break;
-		while(name[j] != '\0')
-		{
-			if(wild_string[i] == name[j])
-				break;
-			j++;
-		}
-		if(name[j] == '\0' && wild_string[i] != '\0')
-			return (1);
-		i++;
-	}
-	while(wild_string[i] != '*')
-		if(name[len--] != wild_string[i--])
-			return (1);
-	return 0;
+	// i = 0;
+	// j = 0;
+	// wild_flag = 0;
+	// while(wild_string[i] != '*' && wild_string[i] != '\0')
+	// {
+	// 	if(object_name[j] != wild_string[i])
+	// 		return (1);
+	// 	i++;
+	// 	j++;
+	// }
+	// while(wild_string[i] != '\0')
+	// {
+	// 	if(wild_string[i] == '*')
+	// 		i++;
+	// 	else
+	// 	{
+	// 		wild_flag = 1;
+	// 		while(object_name[j] != '\0' && wild_string[i] != '\0')
+	// 		{
+	// 			if(wild_string[i] == '*')
+	// 				break;
+	// 			if(object_name[j] == wild_string[i])
+	// 				i++;
+	// 			j++;
+	// 		}
+	// 		while(wild_string[i] == '*')
+	// 			i++;
+	// 		if(object_name[j] == '\0' && wild_string[i] != '\0')
+	// 			return (1);
+	// 	}
+	// }
+	// if(object_name[j] != '\0' && wild_string[i] == '\0' && wild_flag == 1)
+	// 	return (1);
+	return (0);
 }
 
 char	**wildcards(char *wild_string)
@@ -55,14 +56,15 @@ char	**wildcards(char *wild_string)
 		if (entry->d_name[0] == '.')
 			continue ;
 		if (!check_suitable(entry->d_name, wild_string))
-			printf("%s\n", entry->d_name);
+			printf("%s ", entry->d_name);
 	}
+	printf("\n");
 	return NULL;
 }
 
 int main()
 {
-	char **ptr = wildcards("****hell*");
+	char **ptr = wildcards("***file*");
 	// for(int i = 0; ptr[i]; i++)
 	// 	printf("%s\n", ptr[i]);
 }
