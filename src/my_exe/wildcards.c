58245a2f3a0ae2b85cc39bd137c4ca5afc75a8d4
ZoktrFall <shahavetgnum@gmail.com>
2023-04-03 23:48:20 +0400

    50% wildcards

--- src/my_exe/wildcards.c
+++ src/my_exe/wildcards.c
@@ -1,41 +1,39 @@
 #include "minishell.h"
 
+int	check_mid(char **stars, size_t i_object, char *object_name)
+{
+	size_t	count;
+	char 	*addres;
+
+	count = 0;
+	while(stars[count])
+	{
+		if((object_name = ft_strnstr(object_name, stars[count], i_object)) == NULL)
+			return (1);
+		count++;
+	}
+	return (0);
+}
+
 int	check_suitable(char *object_name, char *wild_string)
 {
-	size_t	i;
-	size_t	j;
-	size_t	flag_exit;
-
-	i = 0;
-	j = 0;
-	while(wild_string[i] != '*')
-		if(object_name[j++] != wild_string[i++])
+	size_t	i_wild;
+	size_t	i_object;
+	char	**stars;
+	
+	i_object = ft_strlen(object_name) - 1;
+	i_wild = ft_strlen(wild_string) - 1;
+	while(wild_string[i_wild] != '*')
+		if(wild_string[i_wild--] != object_name[i_object--])
 			return (1);
-	while(wild_string[i] != '\0')
-	{
-		while(wild_string[i] == '*' && wild_string[i] != '\0')
-			i++;
-		if(wild_string[i] == '\0' && flag_exit != 0)
-			break;
-		if((object_name[j] == '\0' && wild_string[i] != '\0') || flag_exit == 0)
+	i_wild = 0;
+	i_object = 0;
+	while(wild_string[i_wild] != '*')
+		if(wild_string[i_wild++] != object_name[i_object++])
 			return (1);
-		flag_exit = 0;
-		while(object_name[j] != '\0')
-		{
-			if(object_name[j] == wild_string[i])
-			{
-				flag_exit = 1;
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	i = ft_strlen(wild_string) - 1;
-	j = ft_strlen(object_name) - 1;
-	while(wild_string[i] != '*')
-		if(wild_string[i--] != object_name[j--])
-			return (1);
+	stars = ft_split(wild_string + i_wild, '*');
+	if(check_mid(stars, ft_strlen(object_name), object_name))
+		return (1);
 	return (0);
 }
 
@@ -53,7 +51,9 @@ char	**wildcards(char *wild_string)
 		if (entry->d_name[0] == '.')
 			continue ;
 		if (!check_suitable(entry->d_name, wild_string))
+		{
 			printf("%s ", entry->d_name);
+		}
 	}
 	printf("\n");
 	return NULL;
@@ -61,7 +61,7 @@ char	**wildcards(char *wild_string)
 
 // int main()
 // {
-// 	char **ptr = wildcards("f*el*");
+// 	char **ptr = wildcards("*li**");
 // 	// for(int i = 0; ptr[i]; i++)
 // 	// 	printf("%s\n", ptr[i]);
 // }
