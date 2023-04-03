b373d547bf74f5fbff2666ac1587864a0e862e4e
ZoktrFall <shahavetgnum@gmail.com>
2023-04-03 16:16:43 +0400

    fixed leak in replace_envp

--- src/my_exe/wildcards.c
+++ src/my_exe/wildcards.c
@@ -1,84 +1,67 @@
-#include "minishell.h"
+// #include "minishell.h"
 
-int	mid_part(size_t i, size_t j, char *object_name, char *wild_string)
-{
-	size_t	flag_exit;
-	size_t	flag[2];
+// int	check_suitable(char *object_name, char *wild_string)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	flag_previous = 0;
 
+// 	i = 0;
+// 	j = 0;
+// 	while(wild_string[i] != '*')
+// 		if(object_name[j++] != wild_string[i++])
+// 			return (1);
+// 	while(wild_string[i] != '\0')
+// 	{
+// 		if(wild_string[i] == '*')
+// 		{
+// 			flag_previous = 0;
+// 			i++;
+// 		}
+// 		else
+// 		{
+// 			while(object_name[j] != '\0')
+// 			{
+// 				if(object_name[j] != wild_string[i] && flag_previous == 1)
+// 					return (1);
+// 				if(object_name[j] == wild_string[i])
+// 				{
+// 					flag_previous = 1;
+// 					break;
+// 				}
+// 				j++;
+// 			}
+// 			if(object_name[j] == '\0' && wild_string[i] != '\0')
+// 				return (1);
+// 			i++;
+// 		}
+// 	}
+// 	return (0);
+// }
 
-	flag[0] = 0;
-	while(wild_string[i] != '\0')
-	{
-		flag[1] = flag[0];
-		while(wild_string[i] == '*' && wild_string[i] != '\0')
-			i++;
-		if(wild_string[i] == '\0' && flag[0] != 0)
-			break;
-		if((object_name[j] == '\0' && wild_string[i] != '\0') || flag[0] == 0)
-			return (1);
-		while(object_name[j] != '\0')
-		{
-			if(object_name[j] != wild_string[i] && flag[0] == 1)
-				return (1);
-			if(object_name[j] == wild_string[i])
-			{
-				flag[0] = 1;
-				break;
-			}
-			j++;
-		}
-		if(flag[0] == 1 && flag[1] == 1)
-		{
-			flag[1] = 0;
-			flag[0] = 0;
-		}
-		i++;
-	}
-	return (0);
-}
-int	check_suitable(char *object_name, char *wild_string)
-{
-	size_t	i;
-	size_t	j;
+// char	**wildcards(char *wild_string)
+// {
+// 	struct dirent	*entry;
+// 	char 			**found_objects;
+// 	DIR				*directory;
 
-	i = 0;
-	j = 0;
-	while(wild_string[i] != '*')
-		if(object_name[j++] != wild_string[i++])
-			return (1);
-	if(mid_part(i, j, object_name, wild_string))
-		return (1);
-	i = ft_strlen(wild_string) - 1;
-	j = ft_strlen(object_name) - 1;
-	while(wild_string[i] != '*')
-		if(wild_string[i--] != object_name[j--])
-			return (1);
-	return (0);
-}
+// 	directory = opendir(".");
+// 	found_objects = NULL;
+// 	entry = readdir(directory);
+// 	while ((entry = readdir(directory)) != NULL)
+// 	{
+// 		if (entry->d_name[0] == '.')
+// 			continue ;
+// 		if (!check_suitable(entry->d_name, wild_string))
+// 			printf("%s ", entry->d_name);
+// 	}
+// 	printf("\n");
+// 	return NULL;
+// }
 
-char	**wildcards(char *wild_string)
-{
-	struct dirent	*entry;
-	char 			**found_objects;
-	DIR				*directory;
-
-	directory = opendir(".");
-	found_objects = NULL;
-	entry = readdir(directory);
-	while ((entry = readdir(directory)) != NULL)
-	{
-		if (entry->d_name[0] == '.')
-			continue ;
-		if (!check_suitable(entry->d_name, wild_string))
-			printf("%s ", entry->d_name);
-	}
-	printf("\n");
-	return NULL;
-}
-
-int main()
-{
-	char **ptr = wildcards("f****");
-	// for(int i = 0; ptr[i]; i++)
-	// 	printf("%s\n", ptr[i]);
-}
+// // int main()
+// // {
+// // 	char **ptr = wildcards("******ile*****");
+// // 	// for(int i = 0; ptr[i]; i++)
+// // 	// 	printf("%s\n", ptr[i]);
+// // }
