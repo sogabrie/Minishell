c45aea88d423f367991ee89c98bb7fd98858b49f
ZoktrFall <shahavetgnum@gmail.com>
2023-04-05 16:45:34 +0400

    update wildcards

--- src/my_exe/wildcards.c
+++ src/my_exe/wildcards.c
@@ -21,42 +21,37 @@ char	*ft_strnstr_strlen(const char *big, const char *lit, size_t len)
 				lit[f] != 0 && (i + f) < len)
 			++f;
 		if (f == l)
-		{
-			// printf("%zu\n", i + ft_strlen(lit));
 			return (bi + i + ft_strlen(lit));
-		}
 		++i;
 	}
 	return (0);
 }
 
-
-int	free_tmp_stars(char ***stars, char **tmp_object_name, \
-						char **tmp_wild_string, int exit)
+void	two_dimensional_mas(char ***str)
 {
 	size_t	i;
 
 	i = 0;
-	// if (*tmp_object_name != NULL)
-	// {
-	// 	free(*tmp_object_name);
-	// 	*tmp_object_name = NULL;
-	// }
-	// if (*tmp_wild_string != NULL)
-	// {
-	// 	free(*tmp_wild_string);
-	// 	*tmp_wild_string = NULL;
-	// }
-	// if (*stars != NULL)
-	// {
-	// 	while ((*stars)[i])
-	// 	{
-	// 		free((*stars)[i]);
-	// 		i++;
-	// 	}
-	// 	free(*stars);
-	// 	*stars = NULL;
-	// }
+	if (*str != NULL)
+	{
+		while ((*str)[i])
+		{
+			free((*str)[i]);
+			i++;
+		}
+		free(*str);
+		*str = NULL;
+	}
+}
+
+int	free_tmp_stars(char **stars, char **tmp_object_name, int exit)
+{
+	if (*tmp_object_name != NULL)
+	{
+		free(*tmp_object_name);
+		*tmp_object_name = NULL;
+	}
+	two_dimensional_mas(&stars);
 	return (exit);
 }
 
@@ -66,11 +61,9 @@ int	check_mid(char **stars, size_t i_object, char *object_name)
 	char	*addres;
 
 	count = 0;
-	// printf("%s\n", object_name);
 	while (stars[count])
 	{
 		object_name = ft_strnstr_strlen(object_name, stars[count], i_object);
-		// printf("object = %s\n", object_name);
 		if (object_name == NULL)
 			return (1);
 		count++;
@@ -105,9 +98,22 @@ char	*replace_wild_string(size_t start, size_t end, char *wild_string)
 	while (start != end)
 		replace_wild_string[i++] = wild_string[start++];
 	replace_wild_string[i] = '\0';
-	free(wild_string);
-	wild_string = NULL;
 	return (replace_wild_string);
+}
+
+char	**split_tmp_wild_string(char *wild_string, \
+				size_t i_wild_start, size_t i_wild_end)
+{
+	char	**stars;
+	char	*tmp_wild_string;
+
+	stars = NULL;
+	tmp_wild_string = NULL;
+	tmp_wild_string = replace_wild_string(i_wild_start, \
+									i_wild_end + 1, wild_string);
+	stars = ft_split(tmp_wild_string, '*');
+	free(tmp_wild_string);
+	return (stars);
 }
 
 int	check_suitable(char *object_name, char *wild_string, \
@@ -118,6 +124,8 @@ int	check_suitable(char *object_name, char *wild_string, \
 	size_t	i_object_start;
 	size_t	i_object_end;
 
+	if(object_name[0] == '.' && wild_string[0] != '.')
+		return (1);
 	i_object_end = ft_strlen(object_name) - 1;
 	i_wild_end = ft_strlen(wild_string) - 1;
 	while (wild_string[i_wild_end] != '*')
@@ -130,16 +138,41 @@ int	check_suitable(char *object_name, char *wild_string, \
 			return (1);
 	tmp_object_name = creat_object_name(i_object_end + 1, \
 									i_object_start, object_name);
-	wild_string = replace_wild_string(i_wild_start, \
-									i_wild_end + 1, wild_string);
-	stars = ft_split(wild_string, '*');
-	// for(int i = 0; stars[i]; i++)
-	// 	printf("%s\n", stars[i]);
+	stars = split_tmp_wild_string(wild_string, i_wild_start, i_wild_end);
 	if (stars == NULL)
 		malloc_error();
 	if (check_mid(stars, ft_strlen(tmp_object_name), tmp_object_name))
-		return (free_tmp_stars(&stars, &tmp_object_name, &wild_string, 1));
-	return (free_tmp_stars(&stars, &tmp_object_name, &wild_string, 0));
+		return (free_tmp_stars(stars, &tmp_object_name, 1));
+	return (free_tmp_stars(stars, &tmp_object_name, 0));
+}
+
+char	**creat_found_objects(char **found_objects, char *name)
+{
+	char	**replace_found_objects;
+	size_t	count;
+
+	count = 0;
+	replace_found_objects = NULL;
+	if (found_objects == NULL)
+	{
+		found_objects = malloc(sizeof(char *) * 2);
+		found_objects[0] = ft_strdup(name);
+		found_objects[1] = NULL;
+		return (found_objects);
+	}
+	while (found_objects[count])
+		count++;
+	replace_found_objects = malloc(sizeof(char *) * (count + 2));
+	count = 0;
+	while (found_objects[count])
+	{
+		replace_found_objects[count] = ft_strdup(found_objects[count]);
+		count++;
+	}
+	two_dimensional_mas(&found_objects);
+	replace_found_objects[count++] = ft_strdup(name);
+	replace_found_objects[count] = NULL;
+	return (replace_found_objects);
 }
 
 char	**wildcards(char *wild_string)
@@ -147,7 +180,6 @@ char	**wildcards(char *wild_string)
 	struct dirent	*entry;
 	char			**found_objects;
 	DIR				*directory;
-	char			*tmp_wild_string;
 
 	directory = opendir(".");
 	found_objects = NULL;
@@ -156,30 +188,25 @@ char	**wildcards(char *wild_string)
 		entry = readdir(directory);
 		if (entry == NULL)
 			break ;
-		if (entry->d_name[0] == '.')
-			continue ;
-		// if (tmp_wild_string != NULL)
-			// free(tmp_wild_string);
-		tmp_wild_string = ft_strdup(wild_string);
-		if(tmp_wild_string == NULL)
-			malloc_error();
-		if (!check_suitable(entry->d_name, tmp_wild_string, NULL, NULL))
-		{
-			printf("%s\n", entry->d_name);
-			// found_objects = creat_found_objects(found_objects, entry->d_name);
-		}
-		// system("leaks minishell");
+		if (!check_suitable(entry->d_name, wild_string, NULL, NULL))
+			found_objects = creat_found_objects(found_objects, entry->d_name);
 	}
-	free(tmp_wild_string);
 	closedir(directory);
-	return (NULL);
+	if (found_objects == NULL)
+		return (NULL);
+	free(wild_string);
+	return (found_objects);
 }
 
 int main()
 {
-	char *str = ft_strdup("file**t.*.txt");
-	wildcards(str);
-	// system("leaks minishell");
-	// for(int i = 0; ptr[i]; i++)
-	// 	printf("%s\n", ptr[i]);
+	char *str = ft_strdup(".*fi*l*e***t.*****.txt");
+	char **ptr = NULL;
+	ptr = wildcards(str);
+	system("leaks minishell");
+	if(ptr != NULL)
+		for(int i = 0; ptr[i]; i++)
+			printf("%s\n", ptr[i]);
+	else
+		printf("clear null\n");	
 }
