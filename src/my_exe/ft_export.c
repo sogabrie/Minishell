338dbc08d72dc96cee7173002811e96328b8dd29
ZoktrFall <shahavetgnum@gmail.com>
2023-04-13 23:19:03 +0400

    working ft_export.c

--- src/my_exe/ft_export.c
+++ src/my_exe/ft_export.c
@@ -1,16 +1,23 @@
 // #include "minishell.h"
 
+// int export_number_error(char *str)
+// {
+//     ft_putstr_fd("minishell: export: ", 2);
+//     write(2, "`", 1);
+//     ft_putstr_fd(str, 2);
+//     write(2, "'", 1);
+//     ft_putendl_fd(": not a valid identifier", 2);
+//     return (1);
+// }
+
 // int valid_variable(char *str)
 // {
 //     size_t  i;
 
 //     i = 0;
 //     if(ft_strchr("0123456789", str[0]))
-//     {
-//         exe_error(str, 98, " export: ");
-//         return (1);
-//     }
-
+//         return (export_number_error(str));
+//     return (0);
 // }
 
 // int ft_export(char **str, char ***envp)
