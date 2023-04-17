32b41bd862fc4bb95535bcca346eca4d39e37eca
ZoktrFall <shahavetgnum@gmail.com>
2023-04-17 21:19:31 +0400

    start ft_echo.c

--- src/my_exe/ft_echo.c
+++ src/my_exe/ft_echo.c
@@ -1,25 +1,8 @@
-#include "minishell.h"
+// #include "minishell.h"
 
 // char	*echo_line(char *line)
 // {
-// 	int	i;
 
-// 	i = 0;
-// 	while (line && line[i])
-// 	{
-// 		if (line[i] == '\'')
-// 		{
-// 			remov_quote(line, i, '\'');
-// 			return (line);
-// 		}
-// 		if (line[i] == '\"')
-// 		{
-// 			remov_quote(line, i, '\"');
-// 			return (line);
-// 		}
-// 		++i;
-// 	}
-// 	return (line);
 // }
 
 // int	init_n(char *str)
@@ -29,16 +12,20 @@
 // 	i = 0;
 // 	if (str[i] == '\'' || str[i] == '\"')
 // 		i++;
+//     printf("i = %zu\n", i);
 // 	if (str[i++] != '-')
 // 		return (0);
+//     printf("i = %zu\n", i);
 // 	while (str[i] != '\0')
 // 	{
+//         printf("str[i] = %c\n", str[i]);
 // 		if ((str[i] == '\'' && str[i + 1] != '\0')
 // 			|| (str[i] == '\"' && str[i + 1] != '\0')
 // 			||  str[i] != 'n')
 // 			return (0);
 // 		i++;
 // 	}
+//     printf("i = %zu\n", i);
 // 	return (1);
 // }
 
@@ -64,7 +51,7 @@
 
 // int main(int argc, char *argv[], char *envp[])
 // {
-// 	char **str = ft_split("oper_echo", ' ');
+// 	char **str = ft_split("\'-nnnnnnnnnnnnnnnnnn\'", ' ');
 // 	int error = ft_echo(str);
 // 	printf("error = %d\n", error);
 
