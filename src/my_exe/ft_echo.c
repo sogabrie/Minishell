c32e4d046fa3edd1ebcbf710e2705104b01b158c
ZoktrFall <shahavetgnum@gmail.com>
2023-04-17 20:56:58 +0400

    Merge

--- src/my_exe/ft_echo.c
+++ src/my_exe/ft_echo.c
@@ -1,68 +1,71 @@
 #include "minishell.h"
-#include "struct.h"
 
-void	cat_char(char *line, int i)
-{
-	while (line[i])
-	{
-		line[i] = line[i + 1];
-		++i;
-	}
-}
+// char	*echo_line(char *line)
+// {
+// 	int	i;
 
-void	remov_quote(char *line, int i, char a)
-{
-	cat_char(line, i);
-	while (line[i])
-	{
-		if (line[i] == a)
-		{
-			cat_char(line, i);
-			return ;
-		}
-		++i;
-	}
-}
+// 	i = 0;
+// 	while (line && line[i])
+// 	{
+// 		if (line[i] == '\'')
+// 		{
+// 			remov_quote(line, i, '\'');
+// 			return (line);
+// 		}
+// 		if (line[i] == '\"')
+// 		{
+// 			remov_quote(line, i, '\"');
+// 			return (line);
+// 		}
+// 		++i;
+// 	}
+// 	return (line);
+// }
 
-char	*echo_line(char *line)
-{
-	int	i;
+// int	init_n(char *str)
+// {
+// 	size_t	i;
+	
+// 	i = 0;
+// 	if (str[i] == '\'' || str[i] == '\"')
+// 		i++;
+// 	if (str[i++] != '-')
+// 		return (0);
+// 	while (str[i] != '\0')
+// 	{
+// 		if ((str[i] == '\'' && str[i + 1] != '\0')
+// 			|| (str[i] == '\"' && str[i + 1] != '\0')
+// 			||  str[i] != 'n')
+// 			return (0);
+// 		i++;
+// 	}
+// 	return (1);
+// }
 
-	i = 0;
-	while (line && line[i])
-	{
-		if (line[i] == '\'')
-		{
-			remov_quote(line, i, '\'');
-			return (line);
-		}
-		if (line[i] == '\"')
-		{
-			remov_quote(line, i, '\"');
-			return (line);
-		}
-		++i;
-	}
-	return (line);
-}
+// int	ft_echo(char **args)
+// {
+// 	char	*line;
+// 	int		flag_n;
+// 	size_t	i;
 
-int	ft_echo(char *line, int fd)
-{
-	int	i;
-	int	error;
+// 	i = 0;
+// 	flag_n = 0;
+// 	flag_n = init_n(args[0]);
+// 	printf("%d\n", flag_n);
+// 	// while (args[i])
+// 	// {
+// 	// 	if(args[i][0] == '\'')
+// 	// 	{
+// 	// 		ft_putstr_fd()
+// 	// 	}
+// 	// 	i++;
+// 	// }
+// }
 
-	i = 0;
-	line = echo_line(line);
-	while (line[i])
-	{
-		error = write(fd, &(line[i++]), 1);
-		if (error != 1)
-		{
-			char *a = strerror(error);
-			//printf("%s\n", a);
-			perror("a");
-			return (error);
-		}
-	}
-	return (0);
-}
+// int main(int argc, char *argv[], char *envp[])
+// {
+// 	char **str = ft_split("oper_echo", ' ');
+// 	int error = ft_echo(str);
+// 	printf("error = %d\n", error);
+
+// }
