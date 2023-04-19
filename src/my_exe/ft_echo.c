b8c155a4c9232459c88602e5c8337f74e7678bfb
ZoktrFall <shahavetgnum@gmail.com>
2023-04-19 12:33:46 +0400

    finish echo ???; working ft_echo.c

--- src/my_exe/ft_echo.c
+++ src/my_exe/ft_echo.c
@@ -1,123 +1,128 @@
-// #include "minishell.h"
+#include "minishell.h"
 
-// size_t	do_scop(char *str, size_t *i, char **envp)
-// {
-// 	size_t	j;
-// 	size_t	count;
-// 	char	*ptr;
+size_t	do_scop(char *str, size_t *i, char **envp)
+{
+	size_t	j;
+	size_t	count;
+	char	*ptr;
 
-// 	j = 0;
-// 	count = 0;
-// 	while (str[j] != '\'')
-// 		j++;
-// 	*i += j;
-// 	ptr = ft_substr(str, 0, j);
-// 	count = ft_strlen(search_envp_in(envp, ptr, ft_strlen(ptr)));
-// 	free(ptr);
-// 	return (count);
-// }
+	j = 0;
+	count = 0;
+	while (str[j] != '\'')
+		j++;
+	*i += j;
+	ptr = ft_substr(str, 0, j);
+	count = ft_strlen(search_envp_in(envp, ptr, ft_strlen(ptr)));
+	free(ptr);
+	return (count);
+}
 
-// int	valid_scop(char *str, size_t *i, char **envp)
-// {
-// 	size_t	j;
-// 	char 	*ptr;
+char	*creat_new_line(char *str, char **envp)
+{
+	size_t	i;
+	size_t	count;
+	char 	*new_line;
 
-// 	j = 0;
-// 	if(!ft_strchr(str, '\'') && !search_envp_in(envp, str, ft_strlen(str)));
-// 		return (0);
-// 	if(!ft_strchr(str, '\'') && search_envp_in(envp, str, ft_strlen(str)));
-// 		return (1);
-// 	while (str[j] && str[j] != '\'')
-// 		j++;
-// 	*i += j;
-// 	ptr = ft_substr(str, 0, j);
-// 	if (!search_envp_in(envp, ptr, ft_strlen(ptr)))
-// 	{
-// 		free(ptr);
-// 		return (0);
-// 	}
-// 	free(ptr);
-// 	return (1);
-// }
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if(ft_strchr(str + i, '\''))
+				count += do_scop(str + i + 1, &i, envp);
+			else
+			{
+				count += ft_strlen(search_envp_in(envp, str + i + 1, ft_strlen(str + i + 1)));
+				i += ft_strlen(str + i + 1);
+			}
+		}
+		count++;
+		i++;
+	}
+	new_line = malloc(sizeof(char) * count);
+    printf("count = %zu\n", count);
+	if (new_line == NULL)
+		malloc_error();
+	return (new_line);
+}
 
-// char	*creat_new_line(char *str, char **envp)
-// {
-// 	size_t	i;
-// 	size_t	count;
-// 	char 	*new_line;
+char *variable(char *str, size_t *i, char **envp)
+{
+    size_t  j;
+    char *ptr;
+    char *variable_in;
 
-// 	i = 0;
-// 	count = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '$')
-// 		{
-// 			if(ft_strchr(str + i, '\''))
-// 				count += do_scop(str + i + 1, &i, envp);
-// 			else
-// 			{
-// 				count += ft_strlen(search_envp_in(envp, str + i + 1, ft_strlen(str + i + 1)));
-// 				i += ft_strlen(str + i + 1);
-// 			}
-// 		}
-// 		count++;
-// 		i++;
-// 	}
-// 	new_line = malloc(sizeof(char) * count);
-// 	if (new_line == NULL)
-// 		malloc_error();
-// 	return (new_line);
-// }
+    j = 0;
+    while(str[j] && str[j] != '\'')
+        j++;
+    ptr = ft_substr(str, 0, j);
+    printf("%s\n", ptr);
+    *i += j + 1;
+    variable_in = search_envp_in(envp, ptr, ft_strlen(ptr));
+    printf("%s\n", variable_in);
+    free(ptr);
+    return (variable_in);
+}
 
-// char	*echo_line(char *line, char **envp)
-// {
-// 	size_t	i;
-// 	size_t	j;
-// 	char	*str;
-// 	char 	*new_line;
+char	*echo_line(char *line, char **envp, char *str, char *new_line)
+{
+	size_t	i;
+	size_t	j;
+    size_t  k;
+    char    *ptr;
 
-// 	i = 0;
-// 	j = 0;
-// 	if (line[i] == '\'')
-// 		return (ft_strtrim(line, "\'"));
-// 	str = ft_strtrim(line, "\"");
-// 	new_line = creat_new_line(str, envp);
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '$' && valid_scop(str + i + 1, &i, envp))
-			
-// 		else
-// 			new_line[j++] = str[i++];
-// 	}
-// 	return (NULL);
-// }
+	i = 0;
+	j = 0;
+    k = 0;
+	if (line[i] == '\'')
+		return (ft_strtrim(line, "\'"));
+	str = ft_strtrim(line, "\"");
+	new_line = creat_new_line(str, envp);
+	while (str[i])
+	{
+		if (str[i] == '$')
+        {
+            ptr = variable(str + i + 1, &i, envp);
+            while(ptr && ptr[k])
+                new_line[j++] = ptr[k++];
+            printf("%c\n", str[i]);
+        }
+		else
+			new_line[j++] = str[i++];
+        k = 0;
+	}
+    new_line[j] = '\0';
+    free(str);
+	return (new_line);
+}
 
-// int	ft_echo(char **args, char **envp)
-// {
-// 	char	*line;
-// 	size_t	i;
+int	ft_echo(char **args, char **envp)
+{
+	char	*line;
+	size_t	i;
 
-// 	i = 0;
-// 	if (args == NULL)
-// 	{
-// 		write(1, "\n", 1);
-// 		return (0);
-// 	}
-// 	while (args[i])
-// 	{
-// 		line = echo_line(args[i], envp);
-// 		printf("%s\n", line);
-// 		i++;
-// 		free(line);
-// 	}
-// 	return (0);
-// }
+	i = 0;
+	if (args == NULL)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (args[i])
+	{
+		line = echo_line(args[i], envp, NULL, NULL);
+		printf("line = %s\n", line);
+		free(line);
+        i++;
+	}
+	return (0);
+}
 
 // int main(int argc, char *argv[], char *envp[])
 // {
 // 	char **my_envp = replace_envp(envp);
-// 	char **str = ft_split("\"$HOME\"", ' ');
+// 	char **str = ft_split("\"asfasf$HOMEa\"", ' ');
 // 	int error = ft_echo(str, my_envp);
-// 	printf("error = %d\n", error);
+// 	// printf("error = %d\n", error);
 
 // }
