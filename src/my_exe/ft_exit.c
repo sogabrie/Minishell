52fb0e327265eee784e339bbefbba2eb3f1a8d7a
ZoktrFall <shahavetgnum@gmail.com>
2023-04-21 20:04:29 +0400

    update control finish ft_Exit

--- src/my_exe/ft_exit.c
+++ src/my_exe/ft_exit.c
@@ -1,64 +1,67 @@
-// #include "minishell.h"
+#include "minishell.h"
 
-// int	overflow_or_not(char *str)
-// {
-// 	int					minus;
-// 	int					i;
-// 	unsigned long long	oper;
+int	overflow_or_not(char *str)
+{
+	int					minus;
+	int					i;
+	unsigned long long	oper;
 
-// 	oper = 0;
-// 	i = 0;
-// 	minus = 1;
-// 	if (str[i] == '-')
-// 	{
-// 		minus = -1;
-// 		i++;
-// 	}
-// 	else if (str[i] == '+')
-// 		i++;
-// 	while (str[i] != '\0')
-// 	{
-// 		oper = oper * 10 + str[i] - '0';
-// 		i++;
-// 	}
-// 	if (minus == -1 && oper > LONG_MIN * -1)
-// 		return (1);
-// 	if (oper > LONG_MAX)
-// 		return (1);
-// 	return (0);	
-// }
+	oper = 0;
+	i = 0;
+	minus = 1;
+	if (str[i] == '-')
+	{
+		minus = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0')
+	{
+		oper = oper * 10 + str[i] - '0';
+		i++;
+	}
+	if (minus == -1 && oper > (unsigned long long)(9223372036854775807) + 1)
+		return (1);
+	if (minus == 1 && oper > LONG_MAX)
+		return (1);
+	return (0);
+}
 
-// int ft_exit(char **args)
-// {
-// 	size_t			count;
-// 	unsigned char	exit_code;
+int	char_args(char *str)
+{
+	int	i;
 
-// 	count = 0;
-// 	if (overflow_or_not(args[0]))
-// 	{
-// 		exe_error(args[0], 276, " exit: ");
-// 		exit(1);
-// 	}
-// 	exit_code = (unsigned char)(ft_atoi(args[0]));
-// 	printf("%d\n", exit_code);
-// 	// while (args[count])
-// 	// 	count++;
-// 	// if(count != 1)
-// 	// {
-// 	// 	write(2, "minishell: exit: too many arguments\n", 36);
-// 	// 	return (1);
-// 	// }
+	i = -1;
+	while (str[++i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (1);
+	}
+	return (0);
+}
 
+int	ft_exit(char **args)
+{
+	size_t			count;
+	unsigned char	exit_code;
 
-// }
-
-// int main()
-// {	
-// 	size_t count = 0;
-// 	char **str = ft_split("-9223372036854775808", ' ');
-// 	int code = ft_exit(str);
-// 	printf("code = %d\n", code);
-// 	return (0);
-// }
-
-// //<20
+	count = 0;
+	if (overflow_or_not(args[0]) || char_args(args[0]))
+	{
+		ft_putendl_fd("exit", 1);
+		exe_error(args[0], 276, " exit: ");
+		exit(255);
+	}
+	while (args[count])
+		count++;
+	if (count != 1)
+	{
+		ft_putendl_fd("exit", 1);
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		return (1);
+	}
+	exit_code = (unsigned char)(ft_atoi(args[0]));
+	ft_putendl_fd("exit", 1);
+	exit(exit_code);
+}
