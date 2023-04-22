2c912eb824fbd78b8d4d8c71442ec376c3958f4c
ZoktrFall <shahavetgnum@gmail.com>
2023-04-22 14:53:22 +0400

    update ft_echo.c; add ft_strjoin_exlusive.c; update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -45,6 +45,7 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_strnstr_strlen(const char *big, const char *lit, size_t len);
+char	*ft_strjoin_exlusive(char *str, char *ptr);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
